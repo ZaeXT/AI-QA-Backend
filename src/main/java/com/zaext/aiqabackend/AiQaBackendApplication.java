32d76910c3d055b1ea998030576ad811f56b127e
Akaze <83901773+ZaeXT@users.noreply.github.com>
2025-11-05 19:13:21 +0800

    Initialize Spring Boot project
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/zaext/aiqabackend/AiQaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zaext.aiqabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiQaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiQaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaext/aiqabackend/AiQaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaext.aiqabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
